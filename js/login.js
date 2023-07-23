7c666ff2891f9d7eb3d9151836a1c05b4b94ca5b
Ibrahim Hassan <hassan.ibb120044@gmail.com>
2023-07-23 19:04:32 +0100

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,10 +1,43 @@
 const form = document.querySelector('form');;
+const username = document.querySelector('#username');
+const pass = document.querySelector('#pass');
+const formDiv = document.querySelectorAll('.form-div');
 
-form.addEventListener('submit', chexckLogin)
-function chexckLogin(event){
-    event.preventDefault();
-    console.dir(event)
-    
-    let username = event.currentTarget.username.value;
-    let password = event.currentTarget.password.value;
+function validate() {
+    if (username.value.trim() === '') {
+        let formDiv = username.parentElement;
+        let errorMsg = formDiv.querySelector('small');
+        errorMsg.style.display = 'block';
+        errorMsg.textContent = "Please provide a valid username";
+        formDiv.classList.add('error');
+        formDiv.classList.remove('success');
+    } else {
+        let formDiv = username.parentElement;
+        let errorMsg = formDiv.querySelector('small');
+        errorMsg.style.display = 'none';
+        formDiv.classList.add('success');
+        formDiv.classList.remove('error');
+    }
+
+    if (pass.value.trim() === '') {
+        let formDiv = pass.parentElement;
+        let errorMsg = formDiv.querySelector('small');
+        errorMsg.style.display = 'block';
+        errorMsg.textContent = "Please provide a valid password";
+        formDiv.classList.add('error');
+        formDiv.classList.remove('success');
+    } else {
+        let formDiv = pass.parentElement;
+        let errorMsg = formDiv.querySelector('small');
+        errorMsg.style.display = 'none';
+        formDiv.classList.add('success');
+        formDiv.classList.remove('error');
+    }
 }
+
+form.addEventListener('submit', checkLogin);
+function checkLogin(event) {
+    event.preventDefault();
+    console.dir(event);
+    validate();
+};
--- js/signup.js
+++ js/signup.js
@@ -1,17 +1,86 @@
-const form = document.querySelector('form');;
+const form = document.querySelector('form');
+const firstname = document.querySelector('#firstname');
+const lastname = document.querySelector('#lastname');
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+const phoneNumber = document.querySelector('#phone-number');
+const emailAddress = document.querySelector('#Email-address');
+const gender = document.querySelector('#gender');
+const small = document.querySelectorAll('small')
+const errorMsg = ['first name field blank', 'last name field blank', 'username field blank', 'password field blank', 'password does not match',
+    'add phone number', 'email field blank', 'add a valid email', 'fill this blank space'];
+
+// console.dir(errorMsg)
+
+
+function submitSignUp() {
+    if (firstname.value.trim() === '') {
+        let form = firstname.parentElement;
+        let errorMsg = form.querySelector('small');
+        errorMsg.style.display = 'block';
+        small.textContent = errorMsg[0];
+        form.classList.add('error');
+        form.classList.remove('success');
+    } else {
+        let form = firstname.parentElement;
+        let errorMsg = form.querySelector('small');
+        errorMsg.style.display = 'none';
+        form.classList.add('success');
+        form.classList.remove('error');
+    }
+
+    if (lastname.value.trim() === '') {
+        let form = lastname.parentElement;
+        let errorMsg = form.querySelector('small');
+        errorMsg.style.display = 'block';
+        small.textContent = errorMsg[1];
+        form.classList.add('error');
+        form.classList.remove('success');
+    } else {
+        let form = lastname.parentElement;
+        let errorMsg = form.querySelector('small');
+        errorMsg.style.display = 'none';
+        form.classList.add('success');
+        form.classList.remove('error');
+    }
+
+    if (username.value.trim() === '') {
+        let form = username.parentElement;
+        let errorMsg = form.querySelector('small');
+        errorMsg.style.display = 'block';
+        small.textContent = errorMsg[2];
+        form.classList.add('error');
+        form.classList.remove('success');
+    } else {
+        let form = username.parentElement;
+        let errorMsg = form.querySelector('small');
+        errorMsg.style.display = 'none';
+        form.classList.add('success');
+        form.classList.remove('error');
+    }
+
+    if (password.value.trim() === '') {
+        let form = password.parentElement;
+        let errorMsg = form.querySelector('small');
+        errorMsg.style.display = 'block';
+        small.textContent = errorMsg[3];
+        form.classList.add('error');
+        form.classList.remove('success');
+    } else {
+        let form = password.parentElement;
+        let errorMsg = form.querySelector('small');
+        errorMsg.style.display = 'none';
+        form.classList.add('success');
+        form.classList.remove('error');
+    }
+}
+
+
 
 form.addEventListener('submit', checkSignup)
-function checkSignup(event){
+function checkSignup(event) {
     event.preventDefault();
     console.dir(event)
-    let firstname = event.currentTarget.firstname.value;
-    let lastname = event.currentTarget.lastname.value;
-    let username = event.currentTarget.username.value;
-    let password = event.currentTarget.password.value;
-    let phoneNumber = event.currentTarget.number.value;
-    let emailAddress = event.currentTarget.emailAddress.value;
-    let gender = event.currentTarget.gender.value;
-    let radio = event.currentTarget.radio.value;
-    let checkbox = event.currentTarget.checkbox.value;
-    let textarea = event.currentTarget.textarea.value;
+    submitSignUp()
 }
